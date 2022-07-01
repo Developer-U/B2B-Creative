db65fe146e15061586c73603e66d3e5a6f837ecd
Developer-U <moiseev.yu@yandex.ru>
2022-07-01 16:46:09 +0300

    Added changes

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,26 +1,8 @@
 import svgSprive from 'gulp-svg-sprite'
 
+
+
 export const svgSprite = () => {
-  return app.gulp.src(app.path.src.svgicons, {})
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: 'SVG',
-        message: 'Error: <%= error.message %>'
-      })
-    ))
-    .pipe(svgSprive({
-      mode: {
-        stack: {
-          sprite: '../icons/sprite.svg',
-          // Превью иконок
-          // example: true
-        }
-      }      
-    }))
+  return app.gulp.src('src/svgicons/sprite.svg', {}) 
     .pipe(app.gulp.dest(app.path.build.images))
 }
-
-// export const svgSprite = () => {
-//   return app.gulp.src('src/svgicons/sprite.svg', {}) 
-//     .pipe(app.gulp.dest(app.path.build.images))
-// }
